8d0fb9c040a823d03e6e4b45f2900c49fc24aba1
devheyrin <beforerin@gmail.com>
2022-11-04 17:08:36 +0900

    Update mongodb-express

---
+++ node/mongodb/mongodb.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const mongoClient = require('mongodb').MongoClient;
+const app = express();
+app.set('port', process.env.Port || 3000);
+app.use(morgan('dev'));
+
+var db;
+var databaseUrl = "mongodb://192.168.0.71:27017";
+
+app.get('/', (req, res) => {
+  res.send("Web Server Started");
+});
+
+app.get('/things', (req, res) => {
+  console.log(mongoClient)
+  mongoClient.connect(databaseUrl, function(err, database) {
+    if (err != null) {
+      res.json({'count' : 0});
+    } else {
+      db = database.db('test');
+      db.collection('things').find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        console.log('result: ');
+        console.log(result);
+        res.json(JSON.stringify(result));
+      });
+    }
+  });
+});
+
+app.get('/things', (req, res) => {
+  mongoClient.connect(databaseUrl, function(err, database) {
+    if (err != null) {
+      res.json({'count' : 0});
+    } else {
+      db = database.db('test');
+      db.collection('things').find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        console.log('result: ');
+        console.log(result);
+        res.json(JSON.stringify(result));
+      });
+    }
+  });
+});
+
+app.get('/traffic', (req, res) => {
+  console.log(mongoClient)
+  mongoClient.connect(databaseUrl, function(err, database) {
+    if (err != null) {
+      res.json({'count' : 0});
+    } else {
+      db = database.db('test');
+      db.collection('traffic').find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        console.log('result: ');
+        console.log(result);
+        res.json(JSON.stringify(result));
+      });
+    }
+  });
+});
+
+app.get('/seoul', (req, res) => {
+  console.log(mongoClient)
+  mongoClient.connect(databaseUrl, function(err, database) {
+    if (err != null) {
+      res.json({'count' : 0});
+    } else {
+      db = database.db('test');
+      db.collection('seoul').find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        console.log('result: ');
+        console.log(result);
+        res.json(JSON.stringify(result));
+      });
+    }
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('3000 Port : Server Started');
+});
